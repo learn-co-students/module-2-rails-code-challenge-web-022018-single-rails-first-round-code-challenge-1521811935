ddb677643206db1678896aeb6961b823263b306d
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-03-23 11:28:22 -0400

    Failed

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,10 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    # byebug
+    @superhero = Superhero.find(params[:id])
+    @superpower = Superpower.find(@superhero.superpower_id)
+  end
 end
--- db/migrate/20170725185425_create_superheroes.rb
+++ db/migrate/20170725185425_create_superheroes.rb
@@ -1,6 +1,7 @@
 class CreateSuperheroes < ActiveRecord::Migration[5.0]
   def change
     create_table :superheroes do |t|
+      t.integer :superpower_id
       t.string :name
       t.string :super_name
 
